10d1ec125de51a5fdfca80c463fa4903aaaeaae6
gy <2649263674@qq.com>
2025-06-23 08:32:25 +0800

    init spring use h5

---
+++ game/src/test/java/com/caicai/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caicai.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
