ad90aa226cab4dc3cedb3d5e98d5e7e1f0b91aa5
zer0san <1323639983@qq.com>
2025-06-24 17:04:48 +0800

    writing combat

--- game/src/main/resources/static/combat.js
+++ game/src/main/resources/static/combat.js
@@ -3,8 +3,10 @@
 function handleNextTurn(){
     fetch("http://localhost:8080/nextTurn").then(
         function(res){
-            var text = document.getElementById("text");
-            text.innerText = res.text().fight;
+            res.json().then(data =>{
+                var text = document.getElementById("text");
+                text.innerText = data.fight
+            })
         }
     );
 }
