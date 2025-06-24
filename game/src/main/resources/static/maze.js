c072f33549873d7b3458bbc438e5e152b9420ecb
zer0san <1323639983@qq.com>
2025-06-24 17:10:16 +0800

    writing combat

--- game/src/main/resources/static/maze.js
+++ game/src/main/resources/static/maze.js
@@ -5,8 +5,8 @@ function handleNextStep() {
             function(res){
                 res.json().then(data=>{
                     var score = document.getElementById('score')
-                    var x = res.text().position.x
-                    var y = res.text().position.y
+                    var x = data["position"].x
+                    var y = data["position"].y
                     var cell = document.getElementById('table').rows[x].cells[y]
                     score.innerText = data.hero.score
                     cell.innerText = "Hero"
